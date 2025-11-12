446e7f768aefebc9bf7db7c677409284f3037267
GamerPerson57 <amlorden21@gmail.com>
2025-11-12 11:04:15 -0500

    more tool box

--- code.js
+++ code.js
@@ -4,13 +4,17 @@ let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
-let letters = ["a", "b", "c"];
+// let myFavoriteLetters = ["a", "b", "c"];
 
 
 // let counts = [0,0,0];
-for(let i = 0; i < 100; i++) {
-//     let randomIndex = getRandomIndex(myFavoriteLetters);
-//     counts[randomIndex] += 1;
+// for(let i = 0; i < 100; i++) {
+// //     let randomIndex = getRandomIndex(myFavoriteLetters);
+// //     counts[randomIndex] += 1;
 
-    console.log(toolbox.getRandomItem(letters));
-}
+//     console.log(toolbox.getRandomItem(myFavoriteLetters));
+// }
+
+
+// let shuffled = toolbox.shuffleArray(myFavoriteLetters);
+// console.log(shuffled);
